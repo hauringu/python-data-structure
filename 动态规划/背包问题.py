161185b6e28a1125704a4f8dd8d9bc5b5538adf5
v_ggghuang <v_ggghuang@tencent.com>
2021-07-01 10:38:09 +0800

    背包问题补充

--- 动态规划/背包问题.py
+++ 动态规划/背包问题.py
@@ -9,8 +9,8 @@
 # 递推式：
 # 1.第n件商品体积大于背包容量，不加入背包 f[n][c] = f[n-1][c]
 # 2.能装第n件商品，但可以选择装，也可以选择不装，取最大价值的组合 f[n][c] = max(f[n-1][c], f[n-1][c-v[i]]+p[i])
-# (value, volume)
-goods = [[2, 2], [4, 3], [3, 5], [7, 5]]
+# (value, volume, stock)
+goods = [[2, 2, 2], [4, 3, 3], [3, 5, 3], [7, 5, 2]]
 
 
 def backpack_0_1_recurison(n, c):
@@ -25,6 +25,71 @@ def backpack_0_1_recurison(n, c):
                    backpack_0_1_recurison(n - 1, c - goods[n - 1][1]) + goods[n - 1][0])
 
 
-# def backpack_0_1(n,c):
+def backpack_0_1(goods, c):
+    n = len(goods)
+    dp = [[0 for _ in range(c + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, c + 1):
+            dp[i][j] = dp[i - 1][j]
+            if j >= goods[i - 1][1]:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - goods[i - 1][1]] + goods[i - 1][0])
+    return dp[n][c]
 
-print(backpack_0_1_recurison(4, 10))
+
+# 01背包问题一维优化
+# dp[v] = max(dp[v], dp[v-vi]+pi)
+# dp[i][v]=max{dp[i-1][v],dp[i-1][v-vi]+pi}
+# 一维递推式中的dp[v-vi]+pi替换掉了二维的dp[i-1][v-vi]+pi，所以需要保证dp[v-vi]是第i-1次循环得到的值，而不是第i次循环得到的值，
+# 如果内层循环仍然升序遍历的话，那么在当前第i次外层循环求dp[v]的时候，dp[v-vi]一定会是当前第i次外层循环计算得出的，因为v-vi < v，升序
+# 遍历会先计算dp[v-vi]，那么这个值代入递推式中计算是错误的。如果内层循环选择降序遍历的话，那么在当前第i次外层循环求dp[v]的时候，dp[v-vi]在
+# 在当前第i次外层循环中还未被计算，但在第i-1次外层循环中以及得到了计算，所以此时的dp[v-vi]，dp[v]满足递推式的要求。
+def backpack_01_2(goods, c):
+    n = len(goods)
+    dp = [0 for _ in range(c + 1)]
+    for i in range(1, n + 1):
+        for j in range(c, 0, -1):
+            dp[j] = max(dp[j], dp[j - goods[i - 1][1]] + goods[i - 1][0])
+    return dp[-1]
+
+
+# 有 N 种物品和一个容量是 V 的背包，每种物品都有无限件可用。
+# 第 i 种物品的体积是 vi，价值是 wi。
+# 求解将哪些物品装入背包，可使这些物品的总体积不超过背包容量，且总价值最大。
+# 输出最大价值。
+def backpack_complete(goods, c):
+    n = len(goods)
+    dp = [[0 for _ in range(c + 1)] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, c + 1):
+            for k in range(0, j // goods[i - 1][1] + 1):
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - k * goods[i - 1][1]] + k * goods[i - 1][0])
+    return dp[-1][-1]
+
+
+def backpack_complete_2(goods, c):
+    n = len(goods)
+    dp = [0 for _ in range(c + 1)]
+    for i in range(1, n + 1):
+        for j in range(c, 0, -1):
+            for k in range(0, j // goods[i - 1][1] + 1):
+                dp[j] = max(dp[j], dp[j - k * goods[i - 1][1]] + k * goods[i - 1][0])
+    return dp[-1]
+
+
+# 有 N 种物品和一个容量是 V 的背包。
+# 第 i 种物品最多有 si 件，每件体积是 vi，价值是 wi。
+# 求解将哪些物品装入背包，可使物品体积总和不超过背包容量，且价值总和最大。
+# 输出最大价值。
+
+def backpack_multi(goods, c):
+    n = len(goods)
+    dp = [0 for _ in range(c + 1)]
+    for i in range(1, n + 1):
+        for j in range(c, 0, -1):
+            kmin = min(j // goods[i - 1][1], goods[i - 1][2])
+            for k in range(0, kmin + 1):
+                dp[j] = max(dp[j], dp[j - k * goods[i - 1][1]] + k * goods[i - 1][0])
+    return dp[-1]
+
+
+print(backpack_multi(goods, 18))
